fe1ac736e872107b8512f77c58d2051a91242c9d
Victor <victor.andre900@gmail.com>
2016-11-28 13:56:24 +0100

    Initialisation 1.0

---
+++ TheBigEvent/TheBigEvent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheBigEvent.Startup))]
+namespace TheBigEvent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
