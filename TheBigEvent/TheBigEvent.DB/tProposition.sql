b8649157576cc141bd22c9647e5d58157bd3dcd9
Victor <victor.andre900@gmail.com>
2017-01-30 11:13:08 +0100

    bdd valid

--- TheBigEvent/TheBigEvent.DB/tProposition.sql
+++ TheBigEvent/TheBigEvent.DB/tProposition.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[tProposition]
+﻿CREATE TABLE [tbe].[tProposition]
 (
-	[PropositionId] INT NOT NULL PRIMARY KEY, 
+	[PropositionId] INT NOT NULL PRIMARY KEY identity(0, 1), 
     [Date] DATETIME2 NULL, 
     [BigSelecteId] INT NULL,
 	CONSTRAINT [FK_tProposition_tBigSelect] FOREIGN KEY ([BigSelecteId]) REFERENCES [tbe].[tBigSelecte]([BigSelecteId])
---
+++ TheBigEvent/TheBigEvent.DB/tValide.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [tbe].[tValide]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity(0, 1), 
+    [PropositionId] INT NULL, 
+    [InviteId] INT NULL,
+	CONSTRAINT [FK_tValide_tProposition] FOREIGN KEY ([PropositionId]) REFERENCES [tbe].[tProposition]([PropositionId]),
+	CONSTRAINT [FK_tValide_tInviteId] FOREIGN KEY ([InviteId]) REFERENCES [tbe].[tInvite]([InviteId])
+
+)
